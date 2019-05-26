00ff4775319ff570e0f9e6915abe61afba83202a
Hamilton <ehamiltonhudson@gmail.com>
2019-05-26 03:15:14 -0400

    slight modifications to chats controller

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -7,35 +7,77 @@ class Api::V1::ChatsController < ApplicationController
     render json: @chats, include: "users,conversations,conversations.user"
   end
 
+#
+#   def create
+#     @chat = Chat.new(chat_params)
+#       if @chat.save
+#         @ownership1 = Subscription.new()
+#         @ownership1.chat_id = @chat.id
+#         @ownership1.user_id = params["sender_id"]
+#         @ownership1.save
+#         @ownership2 = Subscription.new()
+#         @ownership2.chat_id = @chat.id
+#         @ownership2.user_id = params["receiver_id"]
+#         @ownership2.save
+# # byebug
+#         @serialized_data = ActiveModelSerializers::Adapter::Json.new(
+#           ChatSerializer.new(@chat)
+#         ).serializable_hash
+#
+#         ActionCable.server.broadcast(
+#         "current_user_#{current_user.id}",
+#         @serialized_data
+#       )
+#       # byebug
+#         ActionCable.server.broadcast(
+#           "current_user_#{params["receiver_id"]}",
+#           @serialized_data
+#         )
+#           head :ok
+#     end
+#   end
 
   def create
-    @chat = Chat.new(chat_params)
-      if @chat.save
-        @ownership1 = Subscription.new()
-        @ownership1.chat_id = @chat.id
-        @ownership1.user_id = params["sender_id"]
-        @ownership1.save
-        @ownership2 = Subscription.new()
-        @ownership2.chat_id = @chat.id
-        @ownership2.user_id = params["receiver_id"]
-        @ownership2.save
-# byebug
-        @serialized_data = ActiveModelSerializers::Adapter::Json.new(
-          ChatSerializer.new(@chat)
+
+    # 1. Create a new chat in the db.
+    chat = Chat.new(chat_params)
+
+    if chat.save
+
+        # 2. Create each relationship between chat and both users via join table user_chat. Both chat users are passed in params.
+        ownership1 = Subscription.new()
+        ownership1.chat_id = chat.id
+        ownership1.user_id = params["sender_id"]
+        ownership1.save
+        ownership2 = Subscription.new()
+        ownership2.chat_id = chat.id
+        ownership2.user_id = params["receiver_id"]
+        ownership2.save
+
+        # 3. Get the serialized data for the chat as defined in ChatSerializer
+        serialized_data = ActiveModelSerializers::Adapter::Json.new(
+            ChatSerializer.new(chat)
         ).serializable_hash
 
+         # 3. Broadcast new serialized chat to both channel subscribers.
         ActionCable.server.broadcast(
-        "current_user_#{current_user.id}",
-        @serialized_data
-      )
-      # byebug
-        ActionCable.server.broadcast(
-          "current_user_#{params["receiver_id"]}",
-          @serialized_data
+            # Broadcast to general open channel
+            # 'conversations_channel',
+
+            # Broadcast to user/sender private channel
+            "current_user_#{current_user.id}",
+            serialized_data
         )
-          head :ok
+
+        ActionCable.server.broadcast(
+            # Broadcast to user/receiver private channel
+            "current_user_#{params["receiver_id"]}",
+            serialized_data
+        )
+
+        head :ok
     end
-  end
+end
 
   private
 
